c00f49aa0d2e07869ebe128b08b9e9857cb89f53
nathaaaan0204 <sakuuragii@gmail.com>
2023-07-13 17:34:52 +0800

    Hatdog ko malaki

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,10 +4,7 @@ export const Contact = () => {
       <div>
       <h1>Contact</h1>
       <p>
-        Lorem ipsum doFlor sit amet consectetur adipisicing elit. Deleniti
-        explicabo sint esse reiciendis nihil numquam nisi natus ipsum rem
-        excepturi tempora repellat, cumque at maiores iure dicta temporibus
-        atque aliquid?
+        Hatdog ko malaki
       </p>
     </div>
     )
